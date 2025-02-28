ceacad4360096d2c0b6bfdf977a87df7da3e7d75
Umaraliyevna <161109706+Umaraliyevna@users.noreply.github.com>
2025-02-28 22:17:22 +0500

    add movie and main media

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ const videoCarusel = document.querySelector(".videos-carusel");
 const movieList2 = document.getElementById("movie-list-2");
 const textBtn = document.getElementById("text-none");
 textBtn.style.color = "#00254d";
+const videoModal = document.getElementById('video_modal');
 
-const url = "https://api.themoviedb.org/3/trending/all/day?language=en-US";
+const commonUrl = 'https://api.themoviedb.org/3/'
 const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzFkYmRiNTcyM2RiNjk1MzEyZjYwZTJhYWJjZjJjZCIsIm5iZiI6MTczOTAxOTQyMi40Njg5OTk5LCJzdWIiOiI2N2E3NTQ5ZTVmYTQyZDdlNzZmMTEwMmYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ER1dWCes98jF_MVpkt-WMwCKPFe3A41DWqGy1NGQ6gs",
-  },
+    method: 'GET',
+    headers: {accept: 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzFkYmRiNTcyM2RiNjk1MzEyZjYwZTJhYWJjZjJjZCIsIm5iZiI6MTczOTAxOTQyMi40Njg5OTk5LCJzdWIiOiI2N2E3NTQ5ZTVmYTQyZDdlNzZmMTEwMmYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ER1dWCes98jF_MVpkt-WMwCKPFe3A41DWqGy1NGQ6gs'}
 };
+const imgUrl = 'https://media.themoviedb.org/t/p/w220_and_h330_face' 
+
 
 function displayCard(listName) {
+  const url = `${commonUrl}/trending/all/day?language=en-US`;
   fetch(url, options)
     .then((res) => res.json())
     .then((json) => {
@@ -26,9 +26,7 @@ function displayCard(listName) {
                  <img class='card-icon' src='images/icon.svg' alt='icon'>
                   <a href='movie.html?movie=${
                     movie.id
-                  }' style='text-decoration:none;'><img id='card-img' src='https://media.themoviedb.org/t/p/w220_and_h330_face${
-                    movie.poster_path
-                  }' alt="${movie.original_title}"></a>
+                  }' style='text-decoration:none;'><img id='card-img' src='${imgUrl}${movie.poster_path}' alt="${movie.original_title}"></a>
                   <div class="circular-progress">
                       <span class="percentage">${Math.floor(
                         movie.popularity
@@ -45,20 +43,10 @@ function displayCard(listName) {
     })
     .catch((err) => console.error(err));
 }
-displayCard(movieList);
-
-const url2 = "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1";
-const options2 = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzFkYmRiNTcyM2RiNjk1MzEyZjYwZTJhYWJjZjJjZCIsIm5iZiI6MTczOTAxOTQyMi40Njg5OTk5LCJzdWIiOiI2N2E3NTQ5ZTVmYTQyZDdlNzZmMTEwMmYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ER1dWCes98jF_MVpkt-WMwCKPFe3A41DWqGy1NGQ6gs",
-  },
-};
 
 function displayCard2(listName) {
-  fetch(url2, options2)
+  const url = `${commonUrl}tv/popular?language=en-US&page=1`;
+  fetch(url, options)
     .then((res) => res.json())
     .then((json) => {
       json.results?.forEach((movie) => {
@@ -84,7 +72,6 @@ function displayCard2(listName) {
     })
     .catch((err) => console.error(err));
 }
-displayCard2(movieList2);
 
 function changeBtn(selectedBtn, className) {
   textBtn.style.color = "#a3f7bf";
@@ -108,26 +95,33 @@ const options3 = {
 };
 
 function displayVideo() {
-  fetch(url3, options3)
+  const url = `${commonUrl}movie/upcoming?language=en-US&page=1`
+  fetch(url, options)
     .then((res) => res.json())
     .then((json) => {
+      console.log(json);
       json.results.forEach((video) => {
         videoList.innerHTML += `
-                <div class='card-video'>
-                   <img class='video-icon' src='images/icon.svg' alt='icon'>
-                   <img onmouseover="bigImg(this,'https://media.themoviedb.org/t/p/w220_and_h330_face${video.poster_path}')" onmouseout="normalImg(this)" id='video-img' src='https://media.themoviedb.org/t/p/w220_and_h330_face${video.backdrop_path}' alt="${video.original_title}">
-                   <div id='player'>
-                        <i class='bx bx-play'></i>
-                   </div>
-                    <p class='subtitle center'>${video.title}</p>
-                    <i class='date center'>${video.release_date}</i>
-                </div>
-            `;
+          <div class='card-video'>
+              <img class='video-icon' src='images/icon.svg' alt='icon'>
+              <img onmouseover="bigImg(this,'https://media.themoviedb.org/t/p/w220_and_h330_face${video.poster_path}')" onmouseout="normalImg(this)" id='video-img' src='https://media.themoviedb.org/t/p/w220_and_h330_face${video.backdrop_path}' alt="${video.original_title}">
+              <div id='player'>
+                  <i class='bx bx-play'></i>
+              </div>
+              <p class='subtitle center'>${video.title}</p>
+              <i class='date center'>${video.release_date}</i>
+          </div>
+        `;
       });
+      videoModal.innerHTML =`
+        <div>
+          <h3>json</h3>
+        </div>
+      `
     })
     .catch((err) => console.error(err));
 }
-displayVideo();
+
 
 function bigImg(x, url) {
   x.style.transform = "scale(1.1)";
@@ -140,4 +134,10 @@ function normalImg(x) {
   x.style.transform = "scale(1)";
 }
 
+function update(){  
+  displayCard(movieList);
+  displayVideo();  
+  displayCard2(movieList2);
+}
+update();
 
